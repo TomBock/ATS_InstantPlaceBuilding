9b246cdb078f19ded5f7593478f05bca6f7e12d7
Tom <tom@txkgaming.com>
2024-05-19 13:14:38 +0200

    ~ moved building hook to separate file - removed obj committed file

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 using Eremite.Buildings;
 using HarmonyLib;
 using UnityEngine.InputSystem;
@@ -9,6 +10,8 @@ namespace PlaceBuildingsInstantly
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        public static ManualLogSource Log => Instance.Logger;
+
         public static Plugin Instance;
         private Harmony harmony;
 
@@ -17,22 +20,6 @@ namespace PlaceBuildingsInstantly
             Instance = this;
             harmony = Harmony.CreateAndPatchAll(typeof(Plugin));  
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-        }
-
-        [HarmonyPatch(typeof(Building), "PlaceBuilding")]
-        [HarmonyPostfix]
-        private static void HookPlaceBuilding(Building __instance)
-        {
-            if (!Keyboard.current.ctrlKey.isPressed)
-                return;
-
-            Instance.Logger.LogInfo($"Skippping construction process for building {__instance}");
-            
-            _ = new BuildingCreator().CreateCompletedBuilding(
-                __instance.BuildingModel,
-                __instance.BuildingState.field, 
-                __instance.BuildingState.rotation);
-            __instance.Remove(false);
         }
     }
 }
--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace PlaceBuildingsInstantly
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "PlaceBuildingsInstantly";
-        public const string PLUGIN_NAME = "PlaceBuildingsInstantly";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
