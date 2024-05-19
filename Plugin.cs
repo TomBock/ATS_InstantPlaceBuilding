dd396bb979344a503b9ffed094885e22ff894996
Tom <tom@txkgaming.com>
2024-05-19 13:00:15 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using Eremite.Buildings;
+using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace PlaceBuildingsInstantly
+{
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance;
+        private Harmony harmony;
+
+        private void Awake()
+        {
+            Instance = this;
+            harmony = Harmony.CreateAndPatchAll(typeof(Plugin));  
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPatch(typeof(Building), "PlaceBuilding")]
+        [HarmonyPostfix]
+        private static void HookPlaceBuilding(Building __instance)
+        {
+            if (!Keyboard.current.ctrlKey.isPressed)
+                return;
+
+            Instance.Logger.LogInfo($"Skippping construction process for building {__instance}");
+            
+            _ = new BuildingCreator().CreateCompletedBuilding(
+                __instance.BuildingModel,
+                __instance.BuildingState.field, 
+                __instance.BuildingState.rotation);
+            __instance.Remove(false);
+        }
+    }
+}
---
+++ obj/Debug/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace PlaceBuildingsInstantly
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "PlaceBuildingsInstantly";
+        public const string PLUGIN_NAME = "PlaceBuildingsInstantly";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
